e46746166ca8dc2568cfe8098e39ae13c05473f6
mr-ahabib <mr.ahabib261@gmail.com>
2026-04-28 12:53:15 +0600

    2nd push

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
 print("This is github learn class")
+
+
+
+
+# This program prints a message and adds two numbers
+
+print("Hello, world!")
+
+a = 5
+b = 3
+
+sum = a + b
+
+print("The sum is:", sum)
